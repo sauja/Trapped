88cb7fb01170d2f5ba2fc0d6b1dbcad87e692153
sauja <sauja7@gmail.com>
2016-07-08 21:26:53 -0500

    username replacement completed

--- app/src/main/java/com/thehrmsinc/trapped/MainPage.java
+++ app/src/main/java/com/thehrmsinc/trapped/MainPage.java
@@ -3,9 +3,7 @@ package com.thehrmsinc.trapped;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.LightingColorFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -201,7 +199,10 @@ public class MainPage extends AppCompatActivity {
                 breakFlag = true;
                 break;
             }
-            new LongOperation(getRemovedText(options[i].getText()), getString(R.string.protagonist_name), false,clickedButton).execute("");
+            if(options[i].getText().contains("USERNAME"))
+                new LongOperation(getRemovedText(options[i].getText().replace("USERNAME",userName)), getString(R.string.protagonist_name), false,clickedButton).execute("");
+            else
+                new LongOperation(getRemovedText(options[i].getText()), getString(R.string.protagonist_name), false,clickedButton).execute("");
 
 
         }
@@ -298,25 +299,7 @@ public class MainPage extends AppCompatActivity {
                 btn[i].setVisibility(Button.GONE);
             }
         }
-        /*if (questions.length == 1) {
-            btn[0].setVisibility(Button.VISIBLE);
-            btn[0].setClickable(true);
-            btn[1].setVisibility(Button.GONE);
-            btn[2].setVisibility(Button.GONE);
-        } else if (questions.length == 2) {
-            btn[0].setVisibility(Button.VISIBLE);
-            btn[0].setClickable(true);
-            btn[1].setVisibility(Button.VISIBLE);
-            btn[1].setClickable(true);
-            btn[2].setVisibility(Button.GONE);
-        } else {
-            btn[0].setVisibility(Button.VISIBLE);
-            btn[0].setClickable(true);
-            btn[1].setVisibility(Button.VISIBLE);
-            btn[1].setClickable(true);
-            btn[2].setVisibility(Button.VISIBLE);
-            btn[2].setClickable(true);
-        }*/
+
     }
 
     public String getRemovedText(String text) {
@@ -326,13 +309,6 @@ public class MainPage extends AppCompatActivity {
         return text;
     }
 
-    public String getChatSource(String text) {
-        if (text.startsWith("AMENA: "))
-            return "Amena";
-        else if (text.startsWith("UnknownNumber: "))
-            return "Unknown Number";
-        return "";
-    }
 
     @Override
     protected void onStop() {
@@ -358,22 +334,9 @@ public class MainPage extends AppCompatActivity {
         mDecorView.setSystemUiVisibility(uiOptions);
     }
 
-    private void showSystemUI() {
-        mDecorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-    }
 
-    @Override
-    public void onNewIntent(Intent intent) {
-        /*Log.e(TAG, "on New Intent");
-        Bundle extras = intent.getExtras();
-        if(intent.hasExtra("clickedButton"))
-            resumeBlock(intent.getIntExtra("clickedButton",-1));
-*/
 
-    }
+
 
     public void resumeBlock(int clickedButton) {
         btn[0].setClickable(false);
@@ -382,7 +345,7 @@ public class MainPage extends AppCompatActivity {
         Log.e(TAG, "Inside Resume block");
         if (clickedButton != -1) {
             btn[clickedButton].setVisibility(Button.VISIBLE);
-            btn[clickedButton].getBackground().setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFFAA0000));
+           // btn[clickedButton].getBackground().setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFFAA0000));
             Messages row = items.get(items.size() - 1);
             Log.e(TAG, row.getChatText());
             if (settings.contains("block")) {
